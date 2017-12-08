f664654180aa2d2ef040ab67efd98d509b78c71a
Gianluca Guarini <gianluca.guarini@gmail.com>
2017-12-08 17:27:23 +0100

    added: unit test and readme file

--- index.next.js
+++ index.next.js
@@ -2,34 +2,36 @@
  * Cookie handling library - Based on:
  * @link https://github.com/madmurphy/cookies.js/blob/master/cookies.js
  */
-
-
 const BEGINNING_OF_TIME = 'Thu, 01 Jan 1970 00:00:00 GMT'
 const END_OF_TIME = 'Fri, 31 Dec 9999 23:59:59 GMT'
 
+const SAFE_RE = /[-.+*]/g
+const KEYS_BLACKLIST_RE = /^(?:expires|max-age|path|domain|secure)$/i
+
+const REPLACE_RE = /((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g
+const SPLIT_RE = /\s*(?:=[^;]*)?;\s*/
+
+const doc = window.document
 
 /**
  * Encode a key to make it safe for being used within a regex
+ * @private
  * @param {String} key - A cookie identifier
  * @returns {String} The safely encoded key
  */
 function encodeForRegex(key) {
-  return encodeURIComponent(key).replace(/[-.+*]/g, '\\$&')
+  return encodeURIComponent(key).replace(SAFE_RE, '\\$&')
 }
 
 /**
  * Write a key-value pair, also handling Boolean and null values
+ * @private
  * @param {Array} entry - A cookie key-value data pair
  * @returns {String} The compiled key-value pair as a string
  */
 function compileKey([key, value]) {
-  if (value == null || value === false) {
-    return ''
-  }
-
-  if (value === true) {
-    return key
-  }
+  if (value == null || value === false) return ''
+  if (value === true) return key
 
   return `${ key }=${ value }`
 }
@@ -41,24 +43,24 @@ function compileKey([key, value]) {
  * @returns {String} The cookie found or an empty string
  */
 export function readCookie(key) {
-  if (!key) {
-    return null
-  }
+  if (!key) return null
 
   const check = new RegExp(`(?:(?:^|.*;)\\s*${ encodeForRegex(key) }\\s*\\=\\s*([^;]*).*$)|^.*$`)
 
-  return decodeURIComponent(window.document.cookie.replace(check, '$1')) || null
+  return decodeURIComponent(doc.cookie.replace(check, '$1')) || null
 }
 
 /**
  * Write a cookie
- * @param {Object} options - A cookie config object
+ * @param { Object } options - A cookie config object
+ * @returns { String } the new cookies string updated
  */
 export function writeCookie({ key, value, maxAge, expires = Infinity, path, domain, secure }) {
-  if (!key || /^(?:expires|max-age|path|domain|secure)$/i.test(key)) {
+  if (!key || KEYS_BLACKLIST_RE.test(key)) {
     throw new Error('Key was not provided or is invalid')
   }
 
+  // set the expiration
   if (!maxAge) {
     switch (expires.constructor) {
     case Number:
@@ -72,7 +74,7 @@ export function writeCookie({ key, value, maxAge, expires = Infinity, path, doma
     }
   }
 
-  window.document.cookie = Object
+  return doc.cookie = Object
     .entries({
       [encodeURIComponent(key)]: encodeURIComponent(value),
       expires,
@@ -88,10 +90,11 @@ export function writeCookie({ key, value, maxAge, expires = Infinity, path, doma
 
 /**
  * Remove a cookie by setting its expiry date into the past
- * @param {Object} options - A cookie config object
+ * @param { Object } options - A cookie config object
+ * @returns { String } the new cookies string updated
  */
 export function deleteCookie({ key, path, domain }) {
-  writeCookie({
+  return writeCookie({
     key,
     value: '',
     expires: BEGINNING_OF_TIME,
@@ -102,33 +105,21 @@ export function deleteCookie({ key, path, domain }) {
 
 /**
  * Cookie key availability check
- * @param {String} key - A cookie identifier
- * @returns {boolean} Flag that indicates the existence of a key in the cookie
+ * @param { String } key - A cookie identifier
+ * @returns { Boolean } Flag that indicates the existence of a key in the cookie
  */
 export function hasCookie(key) {
-  if (!key) {
-    return false
-  }
+  if (!key) return false
 
-  return new RegExp(`(?:^|;\\s*)${ encodeForRegex(key) }\\s*\\=`).test(window.document.cookie)
+  return new RegExp(`(?:^|;\\s*)${ encodeForRegex(key) }\\s*\\=`).test(doc.cookie)
 }
 
 /**
  * Return all cookie keys
- * @returns {Array} All cookies found or an empty string
+ * @returns { Array } All cookies found or an empty string
  */
-export function cookieKeys() {
-  const replaceRE = /((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g
-  const splitRE = /\s*(?:=[^;]*)?;\s*/
-  const keys = window.document.cookie.replace(replaceRE, '').split(splitRE)
+export function listKeys() {
+  const keys = doc.cookie.replace(REPLACE_RE, '').split(SPLIT_RE)
 
   return keys.map(key => decodeURIComponent(key))
 }
-
-export const cookie = Object.freeze({
-  read: readCookie,
-  write: writeCookie,
-  delete: deleteCookie,
-  has: hasCookie,
-  keys: cookieKeys
-})
--- test.js
+++ test.js
@@ -1,10 +1,36 @@
 require('jsdom-global')()
 const assert = require('assert')
-const cookie = require('./')
-const body = document.body
+const { readCookie, writeCookie, listKeys, deleteCookie } = require('./')
+const doc = window.document
+const DOMAIN = 'foo.com'
+const PATH = '/path'
 
 describe('Bianco cookie', function() {
-  it('stub', function() {
-    assert.equal(typeof cookie, 'object')
+
+  afterEach(function() {
+    listKeys().forEach(key => deleteCookie({ key }))
+    assert.ok(listKeys().length === 0)
+  })
+
+  it('it can read cookies', function() {
+    doc.cookie = 'foo=bar;'
+    assert.equal(readCookie('foo'), 'bar')
+  })
+
+  it('it can set cookies', function() {
+    writeCookie({ key: 'foo', value: 'bar' })
+    assert.equal(readCookie('foo'), 'bar')
+  })
+
+  it('it can list cookies', function() {
+    writeCookie({ key: 'foo', value: 'bar' })
+    writeCookie({ key: 'baz', value: 'buz' })
+    assert.equal(listKeys.length, 2)
+  })
+
+  it('it can delete cookies', function() {
+    writeCookie({ key: 'foo', value: 'bar' })
+    deleteCookie({ key: 'foo' })
+    assert.equal(listKeys.length, 0)
   })
 })
